8026f11322a0cef4f786656dc9f90841eb3b5786
NazifaSadia <nazifas323@gmail.com>
2020-04-19 12:02:10 +0600

    True False explored

---
+++ true-false.js
@@ -0,0 +1,37 @@
+// For Number
+//const age = 4;
+const age = 0;
+if(age){
+    console.log("Condition is true.");
+}
+else{
+    console.log("Condition is false.");
+}
+
+//For string
+const name = "Solaiman";
+if(name){
+    console.log("Condition is true.");
+}
+else{
+    console.log("Condition is false.");
+}
+
+
+// Falsy :
+
+// 1) number = 0
+// 2) string = ""
+// 3) string = null
+// 4) string = NAN  (Not A Number)
+// 5) var = undefined
+// 6) value = false
+
+
+// Truthy :
+// 1) number = !0  (Any number without 0)
+// 2) string = "0"
+// 3) string = " "  (Space)
+// 4) age = []  (Empty Array)
+// 5) age = {}  (Empty Object)
+// 6) value = true
