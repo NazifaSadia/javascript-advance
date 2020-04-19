80698568766ca3b9f8cd44c2e16a204f2ea9f980
NazifaSadia <nazifas323@gmail.com>
2020-04-19 12:36:25 +0600

    null vs undefined clear

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+ // 1
+let pakhi;
+ console.log(pakhi);
+// 2
+function add(num1, num2){
+     console.log(num1 + num2);
+}
+const result = add(2,5);
+console.log(result);
+// 3
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+// 4
+const result = add(13);
+console.log(result);
+// 5
+const premik = {name: "smart dude", phone: 2345};
+console.log(premik.gf);
+// 6
+let fun = undefined;
+console.log(fun);
+// 7
+let ages = [2,4,5];
+console.log(ages[11]);
