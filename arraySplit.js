9e6ba2b84f9ac2129fd47f18f1b20940b2bed799
NazifaSadia <nazifas323@gmail.com>
2020-04-23 00:23:39 +0600

    Array slice, splice, join clear

---
+++ arraySplit.js
@@ -0,0 +1,18 @@
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+// slice()
+// const part = nums.slice(3,7);
+// console.log(part);
+// console.log(nums);
+
+// splice()
+//const remove = nums.splice(3,6);
+//const remove = nums.splice(3,6 , 66, 77);
+//const remove = nums.join(3,6);
+// console.log(remove);
+// console.log(nums);
+
+
+// join()
+const together = nums.join(" ");
+console.log(together);
